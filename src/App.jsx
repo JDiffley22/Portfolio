90ea76dd58b9f20954fe0b65ca592a8ea6af04f1
James Diffley <diffs1991@googlemail.com>
2024-05-30 16:31:36 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import React from "react";
 import "./index.css"
+
 
 const App = () => {
     return (
       <>
-      <h1 className="text-3xl font-bold underline">Portfolio</h1>
-      <p>hello world</p>
+      <h1 class="text-3xl font-bold underline">Portfolio</h1>
+      <div class="font-bold underline">
+        <p>Hello World</p>
+        <p>My Name is James</p>
+        <p class="">I am a Software Developer</p>
+      </div>
+      
       </>
     );
   };
   export default App;
+  
