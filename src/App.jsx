fbed07f7d8d0ad5d2843ae10adc6d2dc7bae01c4
James Diffley <diffs1991@googlemail.com>
2024-06-04 17:06:40 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import "./index.css"
 
-
 const App = () => {
     return (
       <>
-      <h1 class="text-3xl font-bold underline">Portfolio</h1>
-      <div class="font-bold underline">
-        <p>Hello World</p>
-        <p>My Name is James</p>
-        <p class="">I am a Software Developer</p>
+      <nav className="container">
+        <ul className="flex flex-row justify-around">
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+    <section className="container">
+      <h1 className="flex text-center text-3xl font-bold">"Hello World" Im James</h1>
+      <div className=" container font-bold">
+        <div className="container flex justify-between">
+          <h1 className="container">Im a Software Developer in the UK</h1>
+            <div className="container">
+              <p>A passionate programmer who has interests ranging from video games to nature conservation</p>
+           </div>
+        </div>
       </div>
-      
+    </section>
+
+      <div className="container">
+        <div className="flex justify-center">
+        <h3 className="text-3xl">Projects</h3>
+        </div>
+        
+      </div>
       </>
     );
   };
